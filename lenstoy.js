5512260c4a193202e0986160aeb6e765e74ff659
Stuart <gravatar@strudel.org.uk>
2012-07-03 17:08:36 +0100

    Refactoring and changing blur function

--- lenstoy.js
+++ lenstoy.js
@@ -12,10 +12,10 @@ function LensToy(input){
 	// Set some variables
 	this.id = (input && typeof input.id=="string") ? input.id : "LensToy";
 	this.src = (input && typeof input.src=="string") ? input.src : "";
-	this.width = (input && typeof input.width=="number") ? input.width : 100;
-	this.height = (input && typeof input.height=="number") ? input.height : 100;
+	this.width = (input && typeof input.width=="number") ? input.width : parseInt(getStyle(this.id, 'width'), 10);
+	this.height = (input && typeof input.height=="number") ? input.height : parseInt(getStyle(this.id, 'height'), 10);
+	this.theta_e = (input && typeof input.theta=="number") ? input.theta : 10; // Units are detector pixels - about 2 arcsec
 	this.events = {load:"",click:"",mousemove:""};	// Let's define some events
-
 
 	this.img = { complete: false };
 	this.stretch = "linear";
@@ -29,51 +29,12 @@ function LensToy(input){
 
 		if(!this.alpha) return;
 
-		var imageData = this.ctx.createImageData(this.width, this.height);
-
-		// Define some variables outside of the loop
-		var source = { x: e.x, y:e.y };
-		var delta = { x: 0, y: 0 };
-		var pos = 0;
-		var i = 0;
-		var r2 = 0;
-
-		// Loop over x and y. Store 1-D pixel index as i.
-		for (var row = 0 ; row < this.height ; row++){
-			for (var col = 0 ; col < this.width ; col++){
-
-
-				delta.x = col - source.x - this.alpha[i].x;
-				delta.y = row - source.y - this.alpha[i].y;
-
-				r2 = ( delta.x*delta.x + delta.y*delta.y );
-				this.predictedimage[i] = 255*Math.exp(-r2/8);
-
-				// Add to green channel
-				imageData.data[pos+1] = 255;
-
-				// Add to blue channel
-				imageData.data[pos+2] = 255;
-
-				// Alpha channel
-				imageData.data[pos+3] = this.predictedimage[i];
-
-				i++;
-				pos += 4;
-
-			}
-		}
-		// Because of the way putImageData replaces all the pixel values, 
-		// we have to create a temporary canvas and put it there.
-		var overlayCanvas = document.createElement("canvas");
-		overlayCanvas.width = this.width;
-		overlayCanvas.height = this.height;
-		overlayCanvas.getContext("2d").putImageData(imageData, 0, 0);
 		// Paste original image
 		this.pasteFromClipboard();
-		// Now we can combine the new image with our existing canvas
-		// whilst preserving transparency
-		this.ctx.drawImage(overlayCanvas, 0, 0);
+
+		// Draw the lensed image on top
+		this.drawLensImage({ x: e.x, y:e.y });
+
 
 		var r = 5;
 		// Add a circle to show where the source is
@@ -86,7 +47,146 @@ function LensToy(input){
 	});
 
 }
+LensToy.prototype.drawLensImage = function(source){
+	var imageData = this.ctx.createImageData(this.width, this.height);
 
+	// Define some variables outside of the loop
+	var delta = { x: 0, y: 0 };
+	var pos = 0;
+	var i = 0;
+	var r2 = 0;
+
+	// Loop over x and y. Store 1-D pixel index as i.
+	for (var row = 0 ; row < this.height ; row++){
+		for (var col = 0 ; col < this.width ; col++){
+
+			delta.x = col - source.x - this.alpha[i].x;
+			delta.y = row - source.y - this.alpha[i].y;
+
+			r2 = ( delta.x*delta.x + delta.y*delta.y );
+			this.predictedimage[i] = 255*Math.exp(-r2/8);
+
+			// Add to red channel
+			imageData.data[pos+0] = 255;
+			// Add to green channel
+			imageData.data[pos+1] = 255;
+			// Add to blue channel
+			imageData.data[pos+2] = 255;
+
+			// Alpha channel
+			imageData.data[pos+3] = this.predictedimage[i];
+
+			i++;
+			pos += 4;
+		}
+	}
+
+	imageData = this.blur(imageData);
+
+	// Because of the way putImageData replaces all the pixel values, 
+	// we have to create a temporary canvas and put it there.
+	var overlayCanvas = document.createElement("canvas");
+	overlayCanvas.width = this.width;
+	overlayCanvas.height = this.height;
+	overlayCanvas.getContext("2d").putImageData(imageData, 0, 0, this.width, this.height);
+
+	// Now we can combine the new image with our existing canvas
+	// whilst preserving transparency
+	this.ctx.drawImage(overlayCanvas, 0, 0);
+}
+
+LensToy.prototype.blur = function(imageData){
+	//return this.convolve(imageData, this.kernel);
+
+	var scale = 4;
+	var steps = 1;
+	var smallW = Math.round(this.width / scale);
+	var smallH = Math.round(this.height / scale);
+
+	var canvas = document.createElement("canvas");
+	canvas.width = this.width;
+	canvas.height = this.height;
+	var ctx = canvas.getContext("2d");
+	ctx.putImageData(imageData,0,0,this.width,this.height);
+
+	var copy = document.createElement("canvas");
+	copy.width = smallW;
+	copy.height = smallH;
+	var copyCtx = copy.getContext("2d");
+
+	for (var i=0;i<steps;i++) {
+		var scaledW = Math.max(1,Math.round(smallW - i));
+		var scaledH = Math.max(1,Math.round(smallH - i));
+
+		copyCtx.clearRect(0,0,smallW,smallH);
+		copyCtx.drawImage(canvas,0,0,this.width,this.height,0,0,scaledW,scaledH);
+		ctx.drawImage(copy,0,0,scaledW,scaledH,0,0,this.width,this.height);
+	}
+
+	return ctx.getImageData(0,0,this.width,this.height);
+
+}
+LensToy.prototype.convolve = function(pixels, weights, opaque){
+
+	if(!this.kernel){
+		r = 15;
+		r2 = r*r;
+		v = 1/r2;
+		this.kernel = new Array(r2);
+		for(var i = 0 ; i < r2 ; i++){
+			this.kernel[i] = v;
+		}
+	}
+
+	var side = Math.round(Math.sqrt(weights.length));
+	var halfSide = Math.floor(side/2);
+	var src = pixels.data;
+	var sw = pixels.width;
+	var sh = pixels.height;
+	// pad output by the convolution matrix
+	var w = sw;
+	var h = sh;
+	var tmpCanvas = document.createElement('canvas');
+	var tmpCtx = tmpCanvas.getContext('2d');
+	var output = tmpCtx.createImageData(this.width, this.height);
+	var dst = output.data;
+	// go through the destination image pixels
+	var alphaFac = opaque ? 1 : 0;
+	var sy, sx, dstOff, r, g, b, a, wt, srcOff, scy, scx
+	for (var y=0; y<h; y++) {
+		for (var x=0; x<w; x++) {
+			sy = y;
+			sx = x;
+			dstOff = (y*w+x)*4;
+			// calculate the weighed sum of the source image pixels that
+			// fall under the convolution matrix
+			r=0;
+			g=0;
+			b=0;
+			a=0;
+			//if(src[dstOff+3] == 0) continue;
+			for (var cy=0; cy<side; cy++) {
+				for (var cx=0; cx<side; cx++) {
+					scy = sy + cy - halfSide;
+					scx = sx + cx - halfSide;
+					if (scy >= 0 && scy < sh && scx >= 0 && scx < sw) {
+						srcOff = (scy*sw+scx)*4;
+						wt = weights[cy*side+cx];
+						r += src[srcOff] * wt;
+						g += src[srcOff+1] * wt;
+						b += src[srcOff+2] * wt;
+						a += src[srcOff+3] * wt;
+					}
+				}
+			}
+			dst[dstOff] = r;
+			dst[dstOff+1] = g;
+			dst[dstOff+2] = b;
+			dst[dstOff+3] = a + alphaFac*(255-a);
+		}
+	}
+	return output;
+}
 // We need to set up the canvas. This may mean attaching to an existing <div>
 // By the end of this function we have this.ctx available with events attached.
 // Make sure you have set the width/height of the canvas element
@@ -95,7 +195,7 @@ LensToy.prototype.setup = function(id){
 
 	// Now we want to build the <canvas> element that will hold our image
 	var el = document.getElementById(id);
-	if(console && typeof console.log=="function") console.log('setup',el,id)
+	//if(console && typeof console.log=="function") console.log('setup',el,id)
 	if(el!=null){
 		// Look for a <canvas> with the specified ID or fall back on a <div>
 		if(typeof el=="object" && el.tagName != "CANVAS"){
@@ -134,7 +234,7 @@ LensToy.prototype.setup = function(id){
 	var _obj = this;
 	addEvent(this.canvas,"click",function(e){
 		_obj.getCursor(e);
-		_obj.trigger("click",{x:this.cursor.x,y:this.cursor.y});
+		_obj.trigger("click",{x:_obj.cursor.x,y:_obj.cursor.y});
 	});
 	addEvent(this.canvas,"mousemove",function(e){
 		_obj.getCursor(e);
@@ -174,8 +274,6 @@ LensToy.prototype.createPredictedImage = function(){
 	this.copyToClipboard();
 	this.alpha = new Array(this.width*this.height);
 	this.predictedimage = new Array(this.width*this.height);
-
-	var theta_e = 10; // Units are detector pixels - about 2 arcsec
 	
 	for(var i = 0 ; i < this.width*this.height ; i++){
 		var x = i % this.width - this.lens.x;
@@ -184,7 +282,7 @@ LensToy.prototype.createPredictedImage = function(){
 		var cosphi = x/r;
 		var sinphi = y/r;
 		
-		this.alpha[i] = { x: theta_e*cosphi, y: theta_e*sinphi };
+		this.alpha[i] = { x: this.theta_e*cosphi, y: this.theta_e*sinphi };
 	}
 
 	return this;
@@ -214,12 +312,12 @@ LensToy.prototype.pasteFromClipboard = function(){
 
 // Use <canvas> to draw a 2D image
 LensToy.prototype.draw = function(type){
-	if(console && typeof console.log=="function") console.log('draw',this.canvas)
+	//if(console && typeof console.log=="function") console.log('draw',this.canvas)
 	if(!this.ctx) return this;
 
 	this.ctx.clearRect(0,0,this.width,this.height);
-
 //	this.ctx.drawImage(this.img,0,0,this.width,this.height);
+
 	return this;
 }
 
@@ -284,4 +382,15 @@ function trim(s) {
 	s = s.replace(/[ ]{2,}/gi," ");
 	s = s.replace(/\n /,"\n");
 	return s;
+}
+
+// A non-jQuery dependent function to get a style
+function getStyle(el, styleProp) {
+	if (typeof window === 'undefined') return;
+	var style;
+	var el = document.getElementById(el);
+	if (el.currentStyle) style = el.currentStyle[styleProp];
+	else if (window.getComputedStyle) style = document.defaultView.getComputedStyle(el, null).getPropertyValue(styleProp);
+	if (style && style.length === 0) style = null;
+	return style;
 }
